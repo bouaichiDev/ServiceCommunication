82358db96b0989c2f4449d71c1f843be0589feb4
bouaichiDev <bouaichiDev@users.noreply.github.com>
2025-12-15 17:08:59 +0000

    deploy: f97f40bbabb285e1f21ba569b1fee7394024d8ef

---
+++ _next/static/gw5cd4lD5DIgodjU4arp1/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fautomatic-rules\u002F[id]","\u002Fautomatic-rules\u002F[id]\u002Fedit","\u002Fcampaign-details\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
